59b05d13087c113c27e0e63f028e05c6f4b94753
Aashish Shri Ram <aashishshriram1@gmail.com>
2026-07-30 00:03:21 +0530

    Time: 0 ms (100%), Space: 15.1 MB (68.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,21 +5,18 @@ public:
         int r = nums.size() - 1;
 
         while (l <= r) {
-            int mid = l + (r - l) / 2;
+            int mid = ((l + r)/2);
 
             if (nums[mid] == target)
                 return mid;
-
             // Left half is sorted
             if (nums[l] <= nums[mid]) {
-
                 if (nums[l] <= target && target < nums[mid]) {
                     r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
             }
-
             // Right half is sorted
             else {
 
