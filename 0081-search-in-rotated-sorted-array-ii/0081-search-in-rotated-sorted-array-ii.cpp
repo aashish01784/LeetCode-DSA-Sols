abc1e735f098efcd2cc3d4c40c1b0f306cd72fc5
Aashish Shri Ram <aashishshriram1@gmail.com>
2026-07-30 15:20:01 +0530

    Time: 0 ms (100%), Space: 18 MB (46.41%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        while(l<=r){
+            int mid = ((l+r)/2);
+            if(nums[mid]==target){
+                return true;
+            }
+            // it should not contain the duplicates so we should take care of them 
+            if(nums[l]==nums[mid] && nums[mid]==nums[r]){
+                l = l+1;
+                r = r-1;
+                continue;
+            }
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target && target<nums[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
